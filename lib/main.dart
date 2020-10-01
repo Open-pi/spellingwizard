aed596f3e8c24e2a04c56b04d4c136b021ff6c8d
Maistrox9 <youssef.toukebri@protonmail.com>
2020-10-01 08:52:41 +0100

    Add lessons list to each category

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,19 @@ class VerbsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue[900],
       ),
+      body: _verbsListView(),
+    );
+  }
+
+  ListView _verbsListView() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, index) {
+        return ListTile(
+            title: Text('Lesson number $index'),
+            subtitle: Text('Put Small Description Here'),
+            leading: starsIcons());
+      },
     );
   }
 }
@@ -93,11 +106,23 @@ class AnimalsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Animals'),
-        centerTitle: true,
-        backgroundColor: Colors.green[900],
-      ),
+        appBar: AppBar(
+          title: Text('Animals'),
+          centerTitle: true,
+          backgroundColor: Colors.green[900],
+        ),
+        body: _animalsListView());
+  }
+
+  ListView _animalsListView() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, index) {
+        return ListTile(
+            title: Text('Lesson number $index'),
+            subtitle: Text('Put Small Description Here'),
+            leading: starsIcons());
+      },
     );
   }
 }
@@ -106,11 +131,45 @@ class ToolsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tools'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-      ),
+        appBar: AppBar(
+          title: Text('Tools'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+        ),
+        body: _toolsListView());
+  }
+
+  ListView _toolsListView() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, index) {
+        return ListTile(
+            title: Text('Lesson number $index'),
+            subtitle: Text('Put Small Description Here'),
+            leading: starsIcons());
+      },
     );
   }
+}
+
+Container starsIcons() {
+  return Container(
+    width: 50,
+    child: Row(
+      children: <Icon>[
+        Icon(
+          Icons.star,
+          size: 15,
+        ),
+        Icon(
+          Icons.star,
+          size: 20,
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+        )
+      ],
+    ),
+  );
 }
