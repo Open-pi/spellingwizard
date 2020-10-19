f3072248bf09922b0704eae5e198b1c5fdcb9025
Maistrox9 <youssef.toukebri@protonmail.com>
2020-10-19 17:51:18 +0100

    Add Answers list after challenge

--- lib/challenge.dart
+++ lib/challenge.dart
@@ -5,6 +5,7 @@ import 'word.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:SpellingWizard/save.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:flip_card/flip_card.dart';
 import 'dart:io';
 
 class ChallengePage extends StatelessWidget {
@@ -55,6 +56,7 @@ class _ChallengeBodyState extends State<ChallengeBody> {
     'moving to the next word',
     'End of the Challenge',
   ];
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
   int correctAnswers = 0;
   int incorrectAnswers = 0;
   double score = 0;
@@ -63,6 +65,7 @@ class _ChallengeBodyState extends State<ChallengeBody> {
   List scoreColor = [];
   int i = 0;
   int attempt = 3;
+  List<Widget> answerList = [];
   final List<Word> wordList;
   final Color accentColor;
   final Tuple3 prefix;
@@ -198,16 +201,29 @@ class _ChallengeBodyState extends State<ChallengeBody> {
           children: [
             TextFormField(
               controller: textController,
-              onFieldSubmitted: (value) async {
+              onFieldSubmitted: (userWord) async {
                 final path = await savePath();
                 setState(() {
                   bool move = false;
                   bool endOfGame = false;
                   bool stillPages = this.i < this.wordList.length - 1;
                   bool lastPage = this.i == this.wordList.length - 1;
-                  if (this.wordList[this.i].word == value) {
+                  if (this.wordList[this.i].word == userWord) {
                     move = true;
                     if (this.attempt == 3) {
+                      this.answerList.add(Row(
+                            children: [
+                              Text('${this.i + 1} - ',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600)),
+                              Text('${this.wordList[this.i].word}',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.greenAccent[700],
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ));
                       this.correctAnswers++;
                     }
                     if (lastPage) {
@@ -221,6 +237,28 @@ class _ChallengeBodyState extends State<ChallengeBody> {
                     }
                   } else {
                     if (this.attempt == 3) {
+                      this.answerList.add(Row(
+                            children: [
+                              Text('${this.i + 1} - ',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600)),
+                              Text('${this.wordList[this.i].word}',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.greenAccent[700],
+                                      fontWeight: FontWeight.w600)),
+                              Text(' not ',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600)),
+                              Text('$userWord',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.redAccent[700],
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ));
                       this.incorrectAnswers++;
                     }
                     this.attempt--;
@@ -272,7 +310,7 @@ class _ChallengeBodyState extends State<ChallengeBody> {
                       this.scoreMessage = this.scoreMessages[1];
                       this.scoreColor = this.scoreColors[2];
                     }
-                    _createAlertDialog(context);
+                    _resultPopup(context);
                   }
                 });
               },
@@ -318,105 +356,171 @@ class _ChallengeBodyState extends State<ChallengeBody> {
         ),
       );
 
-  _createAlertDialog(BuildContext context) => showDialog(
+  _resultPopup(BuildContext context) => showDialog(
       context: context,
       builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          elevation: 0,
-          child: _buildChild(context),
+        return FlipCard(
+          direction: FlipDirection.HORIZONTAL,
+          key: this.cardKey,
+          flipOnTouch: false,
+          front: _frontChild(context),
+          back: _backChild(context),
         );
       });
 
-  _buildChild(BuildContext context) => Container(
-        height: 395,
-        decoration: BoxDecoration(
-            color: Colors.redAccent,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.all(Radius.circular(12))),
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                child: Column(
-                  children: [
-                    circularSlider(
-                      invalue: this.score,
-                      colors: this.scoreColor,
-                    ),
-                    answersCount(
-                      correct: this.correctAnswers,
-                      incorrect: this.incorrectAnswers,
-                    ),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        'View Answers',
-                        style: TextStyle(color: Colors.blue),
+  _frontChild(BuildContext context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 0,
+        child: Container(
+          height: 395,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomCenter,
+                  stops: [0.01, 1],
+                  colors: [Colors.purple, Colors.deepOrange]),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                  child: Column(
+                    children: [
+                      circularSlider(
+                        invalue: this.score,
+                        colors: this.scoreColor,
                       ),
-                    ),
-                  ],
+                      answersCount(
+                        correct: this.correctAnswers,
+                        incorrect: this.incorrectAnswers,
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          this.cardKey.currentState.toggleCard();
+                        },
+                        child: Text(
+                          'View Answers',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12))),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                '${this.scoreTitle}',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16, left: 16),
+                child: Text(
+                  '${this.scoreMessage}',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
                 ),
               ),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12))),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              '${this.scoreTitle}',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16, left: 16),
-              child: Text(
-                '${this.scoreMessage}',
-                style: TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
+              SizedBox(
+                height: 24,
               ),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Quit'),
+                    textColor: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      return Navigator.of(context).pop(true);
+                    },
+                    child: Text('Retake'),
+                    color: Colors.white,
+                    textColor: Colors.deepOrange,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+
+  _backChild(BuildContext context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 0,
+        child: Container(
+          height: 395,
+          decoration: BoxDecoration(
+              color: Colors.amber[300],
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+            child: Column(
+              children: [
+                Text('Answers',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: this.accentColor,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: this
+                      .answerList
+                      .map((row) => Column(
+                            children: [
+                              row,
+                              SizedBox(
+                                height: 13,
+                              )
+                            ],
+                          ))
+                      .toList(),
+                ),
                 FlatButton(
                   onPressed: () {
-                    Navigator.of(context).pop();
+                    this.cardKey.currentState.toggleCard();
                   },
-                  child: Text('Quit'),
-                  textColor: Colors.white,
+                  child: Text(
+                    'Go back',
+                    style: TextStyle(color: Colors.blue),
+                  ),
                 ),
-                SizedBox(
-                  width: 8,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    return Navigator.of(context).pop(true);
-                  },
-                  child: Text('Retake'),
-                  color: Colors.white,
-                  textColor: Colors.redAccent,
-                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       );
 }
